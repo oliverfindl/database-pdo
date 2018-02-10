d0938ce8a5c3149c0d4c0723d4b2dcd46cf8121d
Oliver Findl <github@findl.sk>
2018-02-10 22:46:36 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # database-pdo
+
+Simple [PHP](https://secure.php.net/) database class based on [PDO](https://secure.php.net/manual/en/book.pdo.php) class. This class adds 2 new methods - for ping and bulk insert, which are usefull for web scrappers. All [PDO](https://secure.php.net/manual/en/book.pdo.php) functionality is preserved.
+
+---
+
+## Install
+
+`git clone https://github.com/oliverfindl/database-pdo.git`
+
+## Usage
+
+* Class initialization is same as for [PDO](https://secure.php.net/manual/en/class.pdo.php) class
+* Ping method doesn't require any argument
+* Insert method has 3 argumens:
+	* Table (required), into which will be data inserted
+	* Data (required), which has to be array of associative arrays
+	* Mode (optional):
+		* \OliverFindl\Database::INSERT_DEFAULT:  
+		`INSERT INTO table (...) VALUES (...), (...), ... ;`
+		* \OliverFindl\Database::INSERT_IGNORE:  
+		`INSERT IGNORE INTO table (...) VALUES (...), (...), ... ;`
+		* \OliverFindl\Database::INSERT_UPDATE:  
+		`INSERT INTO table (...) VALUES (...), (...), ... ON DUPLICATE KEY UPDATE column = VALUES(column), ... ;`
+
+## Example
+
+```php
+require_once("./vendor/database-pdo/database.class.php");
+// use \OliverFindl\Database; // optional, if you want omit namespace in code
+
+try {
+	$db = new \OliverFindl\Database(/* ... */); // same arguments as PDO
+	$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+	$db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+} catch(\Exception $e) {
+	exit($e->getMessage());
+}
+
+// ...
+
+try {
+	$db->ping();
+} catch(\Exception $e) {
+	exit($e->getMessage());
+}
+
+// ...
+
+try {
+	$res = $db->insert("table", $data, \OliverFindl\Database::INSERT_UPDATE);
+	if($res) $id = $db->lastInsertId();
+} catch(\Exception $e) {
+	exit($e->getMessage());
+}
+```
+
+## Requirements
+
+* [PHP 7](https://secure.php.net/manual/en/install.php)
+* [PDO extension](https://secure.php.net/manual/en/pdo.setup.php)
+
+## Notes
+
+This class is compliant with selected [PDO::ERR_MODE](http://php.net/manual/en/pdo.setattribute.php).
+
+---
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ src/database.class.php
@@ -0,0 +1,96 @@
+<?php
+	/**
+	 * database-pdo v1.0.0 (2018-02-10)
+	 * Copyright 2018 Oliver Findl
+	 * @license MIT
+	 */
+
+	namespace OliverFindl;
+
+	class Database {
+		private $pdo;
+		private $args;
+
+		const INSERT_DEFAULT = 0;
+		const INSERT_IGNORE = 1;
+		const INSERT_UPDATE = 2;
+
+		public function __construct() {
+			$this->args = func_get_args();
+			$this->pdo = $this->init();
+		}
+
+		public function __call(string $method, array $args) { //NOTE: return types are not unified
+			return call_user_func_array(array($this->pdo, $method), $args);
+		}
+
+		private function init(): \PDO {
+			return (new \ReflectionClass("PDO"))->newInstanceArgs($this->args);
+		}
+
+		public function ping(): bool {
+			try {
+				$this->pdo->query("SELECT 1;");
+			} catch (\PDOException $e) {
+				$this->pdo = $this->init();
+			}
+	 
+			return true;
+		}
+
+		public function insert(string $table, array $array, int $mode = self::INSERT_DEFAULT): bool {
+			if(empty($table) && $this->error("Table argument cannot be empty.")) return false;
+			if(empty($array) && $this->error("Array argument cannot be empty.")) return false;
+			if(empty($array) || !in_array($mode, array(self::INSERT_DEFAULT, self::INSERT_IGNORE, self::INSERT_UPDATE)) && $this->error("Mode argument value is not valid.")) return false;
+
+			if($this->is_assoc($array)) $array = array($array);
+			$cols = array_keys(reset($array));
+			$keys = $vals = array();
+
+			for($i = 0; $i < count($array); $i++) {
+				if(!is_array($array[$i])) $array[$i] = (array) $array[$i];
+				if(!$this->is_assoc($array[$i]) && $this->error("Array argument must contain only associative arrays.")) return false;
+
+				$keys_part = array();
+				foreach($cols as $col) {
+					$key = ":{$col}_{$i}";
+					$vals[$key] = $array[$i][$col];
+					$keys_part[] = $key;
+				}
+				$keys[] = "(".implode(", ", $keys_part).")";
+			}
+
+			if($mode === self::INSERT_UPDATE) $update = implode(", ", array_map(function(string $col): string { return "{$col} = VALUES({$col})"; }, $cols));
+			$cols = "(".implode(", ", $cols).")";
+			$keys = implode(", ", $keys);
+
+			return $this->pdo->prepare("INSERT".($mode === self::INSERT_IGNORE ? " IGNORE" : "")." INTO {$table} {$cols} VALUES {$keys}".($mode === self::INSERT_UPDATE ? " ON DUPLICATE KEY UPDATE {$update}" : "").";")->execute($vals);
+		}
+
+		private function error(string $error): bool {
+			if(empty($this->pdo) || empty($error)) return false;
+
+			switch($this->pdo->getAttribute(\PDO::ATTR_ERRMODE)) {
+				default:
+				case \PDO::ERRMODE_SILENT: {
+					/* nothing */
+					break;
+				}
+				case \PDO::ERRMODE_WARNING: {
+					trigger_error($error, E_USER_WARNING);
+					break;
+				}
+				case \PDO::ERRMODE_EXCEPTION: {
+					throw new \Exception($error);
+					break;
+				}
+			}
+
+			return error_log($error);
+		}
+
+		private function is_assoc(array $array): bool {
+			return !empty($array) && array_keys($array) !== range(0, count($array) - 1);
+		}
+	}
+?>
